0ee43f3e751c28ef115cd8966e7cacbc46230a60
Mike Setzer <michaelsetzerucf@gmail.com>
2023-08-31 11:50:14 -0400

    Adding breadcrumbs template and people cpt

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-namespace AFMainSite\Theme;
+//namespace AFMainSite\Theme;
 
 define( 'AFMAINSITE_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
 
@@ -44,3 +44,51 @@ if ( ! function_exists( 'theme_breadcrumbs' ) ) {
 		echo '</div>';
 	}
 }
+
+function set_default_page_template() {
+	global $post;
+	if ('page' == $post->post_type && 'auto-draft' == $post->post_status ) {
+		$default_template = locate_template( array( 'template-breadcrumbs.php' ) ); // Change 'breadcrumbs-template.php' to your template's filename
+		if (!empty($default_template)) {
+			update_post_meta($post->ID, '_wp_page_template', 'template-breadcrumbs.php');
+		}
+	}
+}
+
+add_action('admin_init', 'set_default_page_template');
+
+function create_people_post_type() {
+	$labels = array(
+		'name'               => _x( 'People', 'post type general name', 'af-mainsite-child-theme' ),
+		'singular_name'      => _x( 'Person', 'post type singular name', 'af-mainsite-child-theme' ),
+		'menu_name'          => _x( 'People', 'admin menu', 'af-mainsite-child-theme' ),
+		'name_admin_bar'     => _x( 'Person', 'add new on admin bar', 'af-mainsite-child-theme' ),
+		'add_new'            => _x( 'Add New', 'person', 'af-mainsite-child-theme' ),
+		'add_new_item'       => __( 'Add New Person', 'af-mainsite-child-theme' ),
+		'new_item'           => __( 'New Person', 'af-mainsite-child-theme' ),
+		'edit_item'          => __( 'Edit Person', 'af-mainsite-child-theme' ),
+		'view_item'          => __( 'View Person', 'af-mainsite-child-theme' ),
+		'all_items'          => __( 'All People', 'af-mainsite-child-theme' ),
+		'search_items'       => __( 'Search People', 'af-mainsite-child-theme' ),
+		'not_found'          => __( 'No people found.', 'af-mainsite-child-theme' ),
+		'not_found_in_trash' => __( 'No people found in Trash.', 'af-mainsite-child-theme' )
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'people' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 5,
+		'supports'           => array( 'title', 'editor', 'thumbnail' )
+	);
+
+	register_post_type( 'people', $args );
+}
+add_action( 'init', 'create_people_post_type' );
--- template-breadcrumbs.php
+++ template-breadcrumbs.php
@@ -7,7 +7,9 @@
 <?php get_header(); the_post(); ?>
 
 <article class="<?php echo $post->post_status; ?> post-list-item">
-	<?php theme_breadcrumbs(); the_content(); ?>
+	<div class="container mt-4 mt-sm-5 mb-5 pb-sm-4">
+		<?php theme_breadcrumbs(); the_content(); ?>
+	</div>
 </article>
 
 <?php get_footer(); ?>
