5687087e892948e31738c7fcb16cc3178b7a00c5
Mike Setzer <michaelsetzerucf@gmail.com>
2023-10-26 16:31:29 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -111,27 +111,30 @@ add_action('init', 'add_category_meta_boxes');
 //Add shortcode to fetch and display people post types.
 //Ex usage - [af_people layout="leadership"]
 function people_by_category_shortcode($atts) {
+	// Shortcode attributes, with default values.
 	$atts = shortcode_atts(array(
 		'category' => '',
 		'layout' => 'leadership'
 	), $atts);
 
-	$output = '<div class="people-posts">';
+	// Start output buffering.
+	ob_start();
 
+	// Determine the layout style.
 	if ($atts['layout'] === 'team') {
+		// Get all categories except certain ones.
 		$categories = get_categories(array(
 			'orderby' => 'name',
 			'order' => 'ASC',
 			'exclude' => array(get_cat_ID('leadership-team'), get_cat_ID('uncategorized'))
 		));
 
+		// Loop through each category.
 		foreach ($categories as $category) {
-			// Skip the 'leadership-team' category
-			if ( 'leadership-team' === $category->slug ) {
-				continue;
-			}
+			// Skip the 'leadership-team' category.
+			if ('leadership-team' === $category->slug) continue;
 
-			// Fetch posts in the current category, sorted by 'team_order' and then by 'date'
+			// Query arguments.
 			$args = array(
 				'post_type' => 'people',
 				'posts_per_page' => -1,
@@ -143,42 +146,49 @@ function people_by_category_shortcode($atts) {
 				),
 			);
 
+			// New query instance.
 			$query = new WP_Query($args);
 
+			// Check for posts.
 			if ($query->have_posts()) {
-				$output .= '<div class="container pb-3">';
-				$output .= '<h3 class="heading-underline pb-2">' . $category->name . '</h3>';
-				$output .= '<div class="row">';
+				echo '<div class="container pb-3">';
+				echo '<h3 class="heading-underline pb-2">' . esc_html($category->name) . '</h3>';
+				echo '<div class="row">';
 
+				// Loop through each post.
 				while ($query->have_posts()) {
 					$query->the_post();
 
-					// Fetch ACF fields using the specified prefix
+					// Fetch ACF fields using the specified prefix.
 					$team_image = get_field('image');
 					$team_name = get_field('name');
 					$team_title = get_field('title');
 					$team_email = get_field('email');
 					$team_phone = get_field('phone_number');
 
+					// Email and phone blocks, conditional on their presence.
 					$email_block = (!empty($team_email)) ? '<fa class="fa fa-envelope mr-2"></fa> <a href="mailto:' . esc_attr($team_email) . '">' . esc_html($team_email) . '</a><br>' : '';
 					$phone_block = (!empty($team_phone)) ? '<fa class="fa fa-phone mr-2"></fa> <a href="tel:' . esc_attr($team_phone) . '">' . esc_html($team_phone) . '</a>' : '';
 
-					$output .= '
-						<div class="col col-4 col-md-3 p-2 pb-3">
-							<img src="' . esc_url($team_image['url']) . '" alt="' . esc_attr($team_image['alt']) . '" class="img-thumbnail" />
-							<h4 class="mt-2 h6">' . esc_html($team_name) . '</h4>
-							<p class="pb-1 mb-0">' . esc_html($team_title) . '</p>
-							' . $email_block . '
-							' . $phone_block . '
-						</div>';
+					// HTML structure for each person.
+					echo '<div class="col col-4 col-md-3 p-2 pb-3">';
+					if ($team_image) {
+						echo '<img src="' . esc_url($team_image['url']) . '" alt="' . esc_attr($team_image['alt']) . '" class="img-thumbnail" />';
+					}
+					echo '<h4 class="mt-2 h6">' . esc_html($team_name) . '</h4>';
+					echo '<p class="pb-1 mb-0">' . esc_html($team_title) . '</p>';
+					echo $email_block;
+					echo $phone_block;
+					echo '</div>';
 				}
 
-				$output .= '</div></div>';
+				echo '</div></div>';  // Close row and container divs.
 			}
 
-			wp_reset_postdata();
+			wp_reset_postdata();  // Reset the query.
 		}
-	} else {
+	} else {  // 'leadership' layout.
+		// Query arguments.
 		$args = array(
 			'post_type' => 'people',
 			'posts_per_page' => -1,
@@ -189,42 +199,50 @@ function people_by_category_shortcode($atts) {
 			)
 		);
 
+		// Apply category if set.
 		if (!empty($atts['category'])) {
 			$args['category_name'] = $atts['category'];
 		}
 
+		// New query instance.
 		$query = new WP_Query($args);
 
+		// Check for posts.
 		if ($query->have_posts()) {
 			while ($query->have_posts()) {
 				$query->the_post();
 				$post_id = get_the_ID();
 
-				// Assume ACF fields are used
+				// Fetch ACF fields.
+				$team_image = get_field('image', $post_id);
 				$person_name = get_field('name', $post_id);
 				$person_title = get_field('title', $post_id);
 				$person_email = get_field('email', $post_id);
 				$person_phone = get_field('phone_number', $post_id);
 
-				$output .= '
-                <div class="container pb-5">
-                    <div class="row">
-                        <div class="col col-4 col-md-3">' . get_the_post_thumbnail($post_id) . '</div>
-                        <div class="col col-8 col-md-9">
-                            <h3>' . esc_html($person_name) . '</h3>
-                            <p class="pb-3">' . esc_html($person_title) . '</p>
-                            <fa class="fa fa-envelope mr-2"></fa> <a href="mailto:' . esc_attr($person_email) . '">' . esc_html($person_email) . '</a><br>
-                            <fa class="fa fa-phone mr-2"></fa> <a href="tel:' . esc_attr($person_phone) . '">' . esc_html($person_phone) . '</a><br>
-                            <a class="btn btn-primary btn-sm" data-toggle="collapse" href="' . get_permalink($post_id) . '">Bio</a>
-                        </div>
-                    </div>
-                </div>';
+				// Create an image block if the image exists.
+				$image_block = ($team_image) ? '<div class="col col-4 col-md-3">' . get_the_post_thumbnail($post_id) . '</div>' : '';
+
+				// HTML structure for each person in 'leadership' layout.
+				echo '<div class="container pb-5"><div class="row">';
+				echo $image_block;
+				echo '<div class="col col-8 col-md-9">';
+				echo '<h3>' . esc_html($person_name) . '</h3>';
+				echo '<p class="pb-3">' . esc_html($person_title) . '</p>';
+				echo '<fa class="fa fa-envelope mr-2"></fa> <a href="mailto:' . esc_attr($person_email) . '">' . esc_html($person_email) . '</a><br>';
+				echo '<fa class="fa fa-phone mr-2"></fa> <a href="tel:' . esc_attr($person_phone) . '">' . esc_html($person_phone) . '</a><br>';
+				echo '<a class="btn btn-primary btn-sm" data-toggle="collapse" href="' . get_permalink($post_id) . '">Bio</a>';
+				echo '</div></div></div>';  // Close divs for person content and row/container.
+
+				wp_reset_postdata();  // Reset the query.
 			}
-			wp_reset_postdata();
 		}
 	}
 
-	$output .= '</div>';
+	// Get buffer contents and clean buffer.
+	$output = ob_get_clean();
+
+	// Return the HTML content.
 	return $output;
 }
 
