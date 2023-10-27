0e0335f36e74e1c1e816eef55e7a78f8e165b0d7
Mike Setzer <michaelsetzerucf@gmail.com>
2023-10-27 10:52:54 -0400

    Update template-financial-info.php

--- template-financial-info.php
+++ template-financial-info.php
@@ -11,20 +11,23 @@ the_post();
 	<div class="container mt-4 mt-sm-5 mb-5 pb-sm-4">
 		<?php theme_breadcrumbs(); ?>
 		<div class="row">
-			<?php
-			if (has_nav_menu('records-management-menu')) { // Check if the menu location exists
-				$walker_menu = class_exists('BS4_Nav_Walker') ? new BS4_Nav_Walker() : '';
-				wp_nav_menu(array(
-					'theme_location'  => 'financial-information-menu',
-					'container'       => 'nav',
-					'container_class' => 'mb-1',
-					'menu_class'      => 'nav flex-column',
-					'walker'          => $walker_menu,
-				));
-			} else {
-				echo '<p>Menu "financial-information-menu" not defined. Please set it in wp-admin.</p>';
-			}
-			?>
+			<div class="col-12 col-md-3 d-none d-md-block"> <!-- Visible on md viewports and above -->
+				<!-- Sidebar with Navigation Menu for md viewports and above -->
+				<?php
+				if (has_nav_menu('records-management-menu')) { // Check if the menu location exists
+					$walker_menu = class_exists('BS4_Nav_Walker') ? new BS4_Nav_Walker() : '';
+					wp_nav_menu(array(
+						'theme_location'  => 'financial-information-menu',
+						'container'       => 'nav',
+						'container_class' => 'mb-1',
+						'menu_class'      => 'nav flex-column',
+						'walker'          => $walker_menu,
+					));
+				} else {
+					echo '<p>Menu "financial-information-menu" not defined. Please set it in wp-admin.</p>';
+				}
+				?>
+			</div>
 
 			<!-- Main Content Area -->
 			<div class="col-12 col-md-9">
