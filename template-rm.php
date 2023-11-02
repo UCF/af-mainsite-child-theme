83e2125bb5ad8a911771f6d6d6afa2c1485042bf
Mike Setzer <michaelsetzerucf@gmail.com>
2023-11-02 11:10:51 -0400

    Update template-rm.php

--- template-rm.php
+++ template-rm.php
@@ -62,7 +62,7 @@ the_post();
 					// Construct the HTML output for the RMLO section
 					$output = '
 					<div class="col col-12 p-2 pb-3">
-						<img src="' . esc_url($rmlo_image['url']) . '" alt="' . esc_attr($rmlo_image['alt']) . '" class="img-thumbnail rmlo-image" />
+						<img src="' . esc_url($rmlo_image['url']) . '" alt="' . esc_attr($rmlo_image['alt']) . '" class="img-thumbnail rmlo-image rounded-circle border-0" />
 						<h4 class="mt-2 h6">' . esc_html($rmlo_name) . '</h4>
 						<p class="pb-1 mb-0">' . esc_html($rmlo_title) . '</p>
 						' . $email_block . '
